77912f018c2563a9d821f6c245f8602dd421d195
Alexander Pirnarev <sashopirnarev@gmail.com>
2020-09-11 11:34:22 +0300

    add handlechange

--- src/App.js
+++ src/App.js
@@ -9,10 +9,29 @@ class App extends React.Component {
     this.state = {
       todos: todosData,
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((item) => {
+        if (item.id === id) {
+          item.completed = !item.completed;
+        }
+        return item;
+      });
+      return {
+        todos: updatedTodos,
+      };
+    });
+
+    console.log("Changed", id);
+  }
+
   render() {
     const todosComponents = this.state.todos.map((item) => (
-      <TodoItem key={item.id} todo={item} />
+      <TodoItem key={item.id} todo={item} handleChange={this.handleChange} />
     ));
     return <div className="todo-list">{todosComponents}</div>;
   }
