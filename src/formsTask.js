6fa5d95132223cc0cc01a9411ca3c050170b4dce
Alexander Pirnarev <sashopirnarev@gmail.com>
2020-09-11 12:50:04 +0300

    add new objects

--- src/formsTask.js
+++ src/formsTask.js
@@ -7,14 +7,20 @@ class App extends Component {
     this.state = {
       firstName: "",
       lastName: "",
+      isFriendly: true,
+      gender: "",
+      favColor: "red",
     };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
+    const { name, value, type, checked } = event.target;
+
+    type === "checkbox"
+      ? this.setState({ [name]: checked })
+      : this.setState({ [name]: value });
   }
 
   render() {
@@ -35,9 +41,55 @@ class App extends Component {
           placeholder="Last Name"
           onChange={this.handleChange}
         />
+
+        <textarea value={"some val"} onChange={this.handleChange} />
+        <label>
+          <input
+            type="checkbox"
+            name="isFriendly"
+            checked={this.state.isFriendly}
+            onChange={this.handleChange}
+          />
+          is Friendly?
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={this.state.gender === "male"}
+            onChange={this.handleChange}
+          />
+          Male
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={this.state.gender === "female"}
+            onChange={this.handleChange}
+          />
+          Female
+        </label>
+        <br />
+        <label>Favorite Color:</label>
+        <select
+          value={this.state.favColor}
+          onChange={this.handleChange}
+          name="favColor"
+        >
+          <option value="blue">Blue</option>
+          <option value="green">Green</option>
+          <option value="red">Red</option>
+        </select>
+
         <h1>
           {this.state.firstName} {this.state.lastName}
         </h1>
+        <h2>You are a {this.state.gender}</h2>
+        <h2>Your favorite color is {this.state.favColor}</h2>
       </form>
     );
   }
